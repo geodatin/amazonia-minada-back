b0d44472c6b4cc28b236e6fbfaf598e569c6a172
Neto1904 <netohsa66@gmail.com>
2021-09-08 12:10:06 -0300

    Add ranking route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import { InvasionFrequencyController } from '../services/invasionFrequencyRanking/InvasionRankingController'
 import { ListInvasionsController } from '../services/listInvasions/ListInvasionsController'
 import { SearchController } from '../services/search/SearchController'
 
@@ -7,8 +8,13 @@ const router = Router()
 
 const searchController = new SearchController()
 const listInvasionsController = new ListInvasionsController()
+const invasionFrequencyController = new InvasionFrequencyController()
 
 router.get('/search/:searchTerm', searchController.handle)
 router.post('/invasions', listInvasionsController.handle)
+router.get(
+  '/invasions/ranking/:territoryType/:dataType',
+  invasionFrequencyController.handle
+)
 
 export { router }
