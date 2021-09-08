472300ae477752b8adab09e6ef1056381c52951a
Neto1904 <netohsa66@gmail.com>
2021-09-08 12:09:11 -0300

    Add method signature on the interface

--- src/repositories/IInvasionRepository.ts
+++ src/repositories/IInvasionRepository.ts
@@ -1,10 +1,15 @@
 import { IFiltersDTO } from '../dtos/IFiltersDTO'
 import { IInvasionDTO } from '../dtos/IInvasionDTO'
+import { IRequestRankingDTO, IResponseRankingDTO } from '../dtos/IRankingDTO'
 import { ISearchDTO } from '../dtos/ISearchDTO'
 
 interface IInvasionRepository {
   searchCompany(searchTerm: string): Promise<ISearchDTO[]>
   listInvasions(filters: IFiltersDTO): Promise<IInvasionDTO[]>
+  invasionRanking({
+    territoryType,
+    dataType,
+  }: IRequestRankingDTO): Promise<IResponseRankingDTO[]>
 }
 
 export { IInvasionRepository }
